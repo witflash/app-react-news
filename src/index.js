eede6fab5f0bec649be08cf54cab5d7773347bf5
Andrew Mambyk <witflash@gmail.com>
2018-08-06 18:53:30 +0300

    add axios and http request

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+const API_URL = 'http://localhost:3000/articles';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+axios.get(API_URL).then(response => console.log('response', response));
